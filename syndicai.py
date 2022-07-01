f9377817fc229d1b5b3d90df453e3ca7c732f1b4
Margarita <marharyta.obraztsova@gmail.com>
2022-07-01 18:42:52 +0300

    Import classifier here

--- syndicai.py
+++ syndicai.py
@@ -1,15 +1,23 @@
 import torch
-import run_goemotions
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+
 
 
 class PythonPredictor:
     def __init__(self, config):
         """ Download pretrained model. """
-        pass
+        model = AutoModelForSequenceClassification.from_pretrained("monologg/bert-base-cased-goemotions-original")
+        tokenizer = AutoTokenizer.from_pretrained("monologg/bert-base-cased-goemotions-original")
+        classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
+        self.model = model
+        self.tokenizer = tokenizer
+        self.classifier = classifier
+        
 
     def predict(self, payload):
         """ Run a model based on url input. """
 
         # Inference
-        response = run_goemotions(payload["text"])
+        
+        response = self.classifier(payload["text"])
         return response
